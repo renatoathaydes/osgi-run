72856e23701d9e9bbcf74668363afea20bb38d20
Renato Athaydes <renatoathaydes@hotmail.com>
2014-07-15 21:54:10 +0200

    Moved OsgiConfig to its own file

---
+++ osgi-run-core/src/main/groovy/com/athaydes/gradle/osgi/OsgiConfig.groovy
@@ -0,0 +1,25 @@
+package com.athaydes.gradle.osgi
+
+import groovy.transform.ToString
+
+@ToString( includeFields = true )
+class OsgiConfig {
+    protected File outDirFile
+    def outDir = "osgi"
+    def bundles = [ ] + FELIX_GOGO_BUNDLES
+    def osgiMain = FELIX
+    String javaArgs = ""
+    String bundlesPath = 'bundle'
+    String configSettings = 'felix'
+
+    static final String FELIX = 'org.apache.felix:org.apache.felix.main:4.4.0'
+
+    static final String EQUINOX = 'org.eclipse.osgi:org.eclipse.osgi:3.7.1'
+
+    static final FELIX_GOGO_BUNDLES = [
+            'org.apache.felix:org.apache.felix.gogo.runtime:0.12.1',
+            'org.apache.felix:org.apache.felix.gogo.shell:0.10.0',
+            'org.apache.felix:org.apache.felix.gogo.command:0.14.0',
+    ].asImmutable()
+
+}
--- osgi-run-core/src/main/groovy/com/athaydes/gradle/osgi/OsgiRunPlugin.groovy
+++ osgi-run-core/src/main/groovy/com/athaydes/gradle/osgi/OsgiRunPlugin.groovy
@@ -1,6 +1,5 @@
 package com.athaydes.gradle.osgi
 
-import groovy.transform.ToString
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
@@ -55,27 +54,5 @@ class OsgiRunPlugin implements Plugin<Project> {
             osgiRunner.run( project, osgiConfig )
         }
     }
-
-}
-
-@ToString( includeFields = true )
-class OsgiConfig {
-    protected File outDirFile
-    def outDir = "osgi"
-    def bundles = [ ] + FELIX_GOGO_BUNDLES
-    def osgiMain = FELIX
-    String javaArgs = ""
-    String bundlesPath = 'bundle'
-    String configSettings = 'felix'
-
-    static final String FELIX = 'org.apache.felix:org.apache.felix.main:4.4.0'
-
-    static final String EQUINOX = 'org.eclipse.osgi:org.eclipse.osgi:3.7.1'
-
-    static final FELIX_GOGO_BUNDLES = [
-            'org.apache.felix:org.apache.felix.gogo.runtime:0.12.1',
-            'org.apache.felix:org.apache.felix.gogo.shell:0.10.0',
-            'org.apache.felix:org.apache.felix.gogo.command:0.14.0',
-    ].asImmutable()
 
 }
