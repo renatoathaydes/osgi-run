15adba7d8e69a1e68f6bbd70a378bf2188bb2291
Renato Athaydes <renatoathaydes@hotmail.com>
2014-12-07 14:40:21 +0100

    Fixed code-runner-ui compiling tests issues

--- osgi-run-test/ipojo-dosgi/code-runner-ui/src/test/groovy/ipojo/example/code/ui/view/MainViewTest.groovy
+++ osgi-run-test/ipojo-dosgi/code-runner-ui/src/test/groovy/ipojo/example/code/ui/view/MainViewTest.groovy
@@ -1,11 +1,17 @@
 package ipojo.example.code.ui.view
 
 import ipojo.example.code.CompositeCodeRunner
+import org.junit.Test
 
 /**
  *
  */
 class MainViewTest {
+
+    @Test
+    void test1() {
+        // just for compiling
+    }
 
     public static void main( String[] args ) {
         final view = new MainView( new CompositeCodeRunner() {
