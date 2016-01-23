72785cb0dcc7ac9fe820ef39656980d098c6ac57
Cristian Prieto <me@cprieto.com>
2016-01-23 12:21:28 +1100

    Updated felix dependencies

--- osgi-run-core/src/main/groovy/com/athaydes/gradle/osgi/OsgiConfig.groovy
+++ osgi-run-core/src/main/groovy/com/athaydes/gradle/osgi/OsgiConfig.groovy
@@ -84,26 +84,26 @@ class OsgiConfig {
 
     // CONSTANTS
 
-    static final String FELIX = 'org.apache.felix:org.apache.felix.main:4.4.0'
+    static final String FELIX = 'org.apache.felix:org.apache.felix.main:5.4.0'
 
     static final String EQUINOX = 'org.eclipse.osgi:org.eclipse.osgi:3.7.1'
 
     static final String KNOPFLERFISH = 'org.knopflerfish:framework:7.1.2'
 
     static final FELIX_GOGO_BUNDLES = [
-            'org.apache.felix:org.apache.felix.gogo.runtime:0.12.1',
-            'org.apache.felix:org.apache.felix.gogo.shell:0.10.0',
-            'org.apache.felix:org.apache.felix.gogo.command:0.14.0',
+            'org.apache.felix:org.apache.felix.gogo.runtime:0.16.2',
+            'org.apache.felix:org.apache.felix.gogo.shell:0.12.0',
+            'org.apache.felix:org.apache.felix.gogo.command:0.16.0',
     ].asImmutable()
 
     static final IPOJO_BUNDLE = [
-            'org.apache.felix:org.apache.felix.ipojo:1.12.0'
+            'org.apache.felix:org.apache.felix.ipojo:1.12.1'
     ].asImmutable()
 
     static final IPOJO_ALL_BUNDLES = IPOJO_BUNDLE + [
             'org.apache.felix:org.apache.felix.shell:1.4.3',
             'org.apache.felix:org.apache.felix.shell.tui:1.4.1',
-            'org.apache.felix:org.apache.felix.bundlerepository:1.6.0',
+            'org.apache.felix:org.apache.felix.bundlerepository:2.0.6',
             'org.apache.felix:org.apache.felix.ipojo.arch:1.6.0'
     ].asImmutable()
 
