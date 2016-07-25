2ac1378b27752acd896663309ae023fe1f6f462b
renato <renato@twobo.com>
2016-07-25 20:56:00 +0200

    Tests for configurations

--- osgi-run-core/src/test/groovy/com/athaydes/gradle/osgi/OsgiRunPluginTest.groovy
+++ osgi-run-core/src/test/groovy/com/athaydes/gradle/osgi/OsgiRunPluginTest.groovy
@@ -13,4 +13,13 @@ class OsgiRunPluginTest {
         assert project.tasks.runOsgi
         assert project.tasks.createOsgiRuntime
     }
+
+    @Test
+    void allConfigurationsAdded() {
+        def project = ProjectBuilder.builder().build()
+        project.apply plugin: 'com.athaydes.osgi-run'
+        assert project.configurations.findByName( 'osgiRuntime' )
+        assert project.configurations.findByName( 'osgiMain' )
+        assert project.configurations.findByName( 'systemLib' )
+    }
 }
